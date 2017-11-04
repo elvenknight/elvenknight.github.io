5eacfae1bf33765eda116d0e47482ec65f0db88c
michael <elvenknight@mflores011@yahoo.com>
2017-11-03 22:41:31 -0500

    Initial commit

--- themes/ruri-dark/mascots.js
+++ themes/ruri-dark/mascots.js
@@ -1,1 +1,1 @@
-var mascotList = [ "ruri1.png", "ruri2.png", "ruri3.png" ];
+var mascotList = [ "arcueid1.png", "haruhi1.png", "kurisu1.png", "ruri1.png", "saber1.png", "crab1.png", "ikaros1.png", "kurisu2.png", "ruri2.png", "crab2.png", "holo2.png", "kotomi1.png", "ruri3.png", "yaya1.png" ];
